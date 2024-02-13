40b96f5f5a0a6c015298035d7ffe61cf63a1aae6
tracynamayi <159949105+tracynamayi@users.noreply.github.com>
2024-02-13 23:23:31 +0300

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+
+    // Check if num is greater than 0
+    if (num > 0) {
+        printf("The number is positive\n");
+    }
+
+    // Check if num is even or odd
+    if (num % 2 == 0) {
+        printf("The number is even\n");
+    } else {
+        printf("The number is odd\n");
+    }
+
+    return 0;
+}
